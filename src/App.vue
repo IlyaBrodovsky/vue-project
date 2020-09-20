917ddb328172ac988bf9f5c863f5e305a8a5345d
IlyaBrodovsky <68086935+IlyaBrodovsky@users.noreply.github.com>
2020-09-20 12:11:34 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  <card :message='message'/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from '@/components/Card'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+   components: {
+    Card
+  },
+  data() {
+    return {
+      message: 'Menu'
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
